cc3bb4e07f1385932f2aa05f73b900e950f37ffb
TheKleyn <104390108+TheKleyn@users.noreply.github.com>
2022-09-15 17:56:08 -0400

    all three files working

--- messages.js
+++ messages.js
@@ -1,8 +1,8 @@
 // mixed messages
 
 const times = ['Today', 'Tomorrow', 'In two weeks', 'Next year', 'In ten years', 'Next month', 'In a fortnight'];
-const subject = ['you', 'your mom', 'your dog', 'your parrot', 'your spouse', 'your boss', 'your grandpa'];
-const adjective = ['a great', 'a tiny', 'a gargantuan', 'an enourmous', 'a microscopic', 'an eternal', 'a resplendid'];
+const subject = ['you', 'your mom', 'your dog', 'your great aunt', 'your spouse', 'your boss', 'your grandpa'];
+const adjective = ['a great', 'a hairy', 'a gargantuan', 'an enourmous', 'a microscopic', 'an eternal', 'a resplendid'];
 const verb = ['experience', 'behold', 'see', 'sense', 'feel', 'percieve', 'ken'];
 const action = ['rubbing of parts', 'flatulance', 'existential crisis', 'menapause', 
 'hair ball', 'revelation of Mosiath', 'visitaion of Jemendel'];
