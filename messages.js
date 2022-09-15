151d18edd52093d0634d706281b55bca33bb2dee
TheKleyn <104390108+TheKleyn@users.noreply.github.com>
2022-09-15 11:39:43 -0400

    completed the arrays

--- messages.js
+++ messages.js
@@ -1,1 +1,9 @@
 // mixed messages
+
+const times = ['today', 'tomorrow', 'in tow weeks', 'next year', 'in ten years', 'next month'];
+const subject = ['you', 'your mom', 'your dog', 'your parrot', 'your spouse', 'your boss'];
+const adjective = ['a great', 'a tiny', 'a gargantuan', 'an enourmous', 'a microscopic', 'an eternal', 'a resplendid'];
+const verb = ['experience', 'behold', 'see', 'sense', 'feel'];
+const action = ['rubbing of parts', 'flatulance', 'existential crisis', 'menapause', 
+'hair ball', 'revelation of Mosiath', 'visitaion of Jemendel'];
+
