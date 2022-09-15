5fda527a671ec8a83ff39bf9a151bb3fe481f383
TheKleyn <104390108+TheKleyn@users.noreply.github.com>
2022-09-15 11:48:36 -0400

    adding function

--- messages.js
+++ messages.js
@@ -1,9 +1,17 @@
 // mixed messages
 
-const times = ['today', 'tomorrow', 'in tow weeks', 'next year', 'in ten years', 'next month'];
-const subject = ['you', 'your mom', 'your dog', 'your parrot', 'your spouse', 'your boss'];
+const times = ['today', 'tomorrow', 'in tow weeks', 'next year', 'in ten years', 'next month', 'in a fortnight'];
+const subject = ['you', 'your mom', 'your dog', 'your parrot', 'your spouse', 'your boss', 'your grandpa'];
 const adjective = ['a great', 'a tiny', 'a gargantuan', 'an enourmous', 'a microscopic', 'an eternal', 'a resplendid'];
-const verb = ['experience', 'behold', 'see', 'sense', 'feel'];
+const verb = ['experience', 'behold', 'see', 'sense', 'feel', 'percieve', 'ken'];
 const action = ['rubbing of parts', 'flatulance', 'existential crisis', 'menapause', 
 'hair ball', 'revelation of Mosiath', 'visitaion of Jemendel'];
 
+const generator = () => {
+    let randNum = Math.floor(Math.random() * 7);
+    console.log(randNum);
+    
+    
+}
+
+console.log(generator());
